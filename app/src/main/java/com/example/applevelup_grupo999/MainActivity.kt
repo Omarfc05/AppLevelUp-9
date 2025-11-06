3e535821109c7cb21e34d423bc5de99ea755f912
Omarff <om.filun@duocuc.cl>
2025-11-06 11:02:06 -0300

    Inicio de proyecto + estructura base para consumo de API REST

---
+++ app/src/main/java/com/example/applevelup_grupo999/MainActivity.kt
@@ -0,0 +1,47 @@
+package com.example.applevelup_grupo999
+
+import android.os.Bundle
+import androidx.activity.ComponentActivity
+import androidx.activity.compose.setContent
+import androidx.activity.enableEdgeToEdge
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.padding
+import androidx.compose.material3.Scaffold
+import androidx.compose.material3.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.tooling.preview.Preview
+import com.example.applevelup_grupo999.ui.theme.AppLevelUp_Grupo999Theme
+
+class MainActivity : ComponentActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        enableEdgeToEdge()
+        setContent {
+            AppLevelUp_Grupo999Theme {
+                Scaffold(modifier = Modifier.fillMaxSize()) { innerPadding ->
+                    Greeting(
+                        name = "Android",
+                        modifier = Modifier.padding(innerPadding)
+                    )
+                }
+            }
+        }
+    }
+}
+
+@Composable
+fun Greeting(name: String, modifier: Modifier = Modifier) {
+    Text(
+        text = "Hello $name!",
+        modifier = modifier
+    )
+}
+
+@Preview(showBackground = true)
+@Composable
+fun GreetingPreview() {
+    AppLevelUp_Grupo999Theme {
+        Greeting("Android")
+    }
+}
